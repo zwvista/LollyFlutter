f9e2515f53a4de1753b669042298ba79d65b8050
zwvista <zwvista@msn.com>
2020-09-15 16:31:11 +0900

    models

---
+++ lib/models/wpp/mwordfami.dart
@@ -0,0 +1,29 @@
+import 'package:json_annotation/json_annotation.dart';
+
+part 'mwordfami.g.dart';
+
+@JsonSerializable()
+class MWordsFami {
+  @JsonKey(name: 'records')
+  List<MWordFami> lst;
+
+  MWordsFami() {}
+  factory MWordsFami.fromJson(Map<String, dynamic> json) => _$MWordsFamiFromJson(json);
+  Map<String, dynamic> toJson() => _$MWordsFamiToJson(this);
+}
+
+@JsonSerializable()
+class MWordFami {
+  @JsonKey(name: 'ID')
+  var id = 0;
+  @JsonKey(name: 'USERID')
+  var userid = 0;
+  @JsonKey(name: 'CORRECT')
+  var correct = 0;
+  @JsonKey(name: 'TOTAL')
+  var total = 0;
+
+  MWordFami() {}
+  factory MWordFami.fromJson(Map<String, dynamic> json) => _$MWordFamiFromJson(json);
+  Map<String, dynamic> toJson() => _$MWordFamiToJson(this);
+}
---
+++ lib/models/wpp/mwordfami.g.dart
@@ -0,0 +1,35 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'mwordfami.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+MWordsFami _$MWordsFamiFromJson(Map<String, dynamic> json) {
+  return MWordsFami()
+    ..lst = (json['records'] as List)
+        ?.map((e) =>
+            e == null ? null : MWordFami.fromJson(e as Map<String, dynamic>))
+        ?.toList();
+}
+
+Map<String, dynamic> _$MWordsFamiToJson(MWordsFami instance) =>
+    <String, dynamic>{
+      'records': instance.lst,
+    };
+
+MWordFami _$MWordFamiFromJson(Map<String, dynamic> json) {
+  return MWordFami()
+    ..id = json['ID'] as int
+    ..userid = json['USERID'] as int
+    ..correct = json['CORRECT'] as int
+    ..total = json['TOTAL'] as int;
+}
+
+Map<String, dynamic> _$MWordFamiToJson(MWordFami instance) => <String, dynamic>{
+      'ID': instance.id,
+      'USERID': instance.userid,
+      'CORRECT': instance.correct,
+      'TOTAL': instance.total,
+    };
