d8bfe97b610b582cffabb7b75bc37b0a6e0a77e8
趙偉 <zwvista@msn.com>
2021-01-30 10:01:50 +0900

    batch 4

--- lib/pages/words/words_unit_batch_edit_page.dart
+++ lib/pages/words/words_unit_batch_edit_page.dart
@@ -15,66 +15,84 @@ class WordsUnitBatchEditPage extends StatefulWidget {
 }
 
 class WordsUnitBatchEditPageState extends State<WordsUnitBatchEditPage> {
-  final _formKey = GlobalKey<FormState>();
   WordsUnitBatchEditViewModel get vmBatch => widget.vmBatch;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(title: Text('Words in Unit(Detail)')),
+        appBar: AppBar(title: Text('Words in Unit(Batch Edit)'), actions: [
+          TextButton(
+            child: Text("Save"),
+            style: TextButton.styleFrom(
+              primary: Colors.white,
+            ),
+            onPressed: () {
+              Navigator.pop(context);
+            },
+          )
+        ]),
         body: Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Form(
-                key: _formKey,
-                child: ListView(
-                  children: [
-                    Row(
-                      children: [
-                        DropdownButtonFormField(
-                            value: vmBatch.unit,
-                            items: vmBatch.textbook.lstUnits
-                                .map((o) => DropdownMenuItem(
-                                    value: o.value, child: Text(o.label)))
-                                .toList(),
-                            decoration: InputDecoration(
-                              labelText: "UNIT",
-                            ),
-                            onChanged: (v) => v,
-                            onSaved: (v) => vmBatch.unit = v),
-                      ],
-                    ),
-                    DropdownButtonFormField(
+            padding: const EdgeInsets.all(10.0),
+            child: Column(
+              children: [
+                Row(children: [
+                  SizedBox(
+                      height: 24.0,
+                      width: 30.0,
+                      child: Checkbox(
+                          value: vmBatch.unitIsChecked,
+                          onChanged: (v) => vmBatch.unitIsChecked = v)),
+                  Expanded(
+                    child: DropdownButtonFormField(
+                        value: vmBatch.unit,
+                        decoration: InputDecoration(
+                          labelText: "UNIT",
+                        ),
+                        items: vmBatch.textbook.lstUnits
+                            .map((o) => DropdownMenuItem(
+                                value: o.value, child: Text(o.label)))
+                            .toList(),
+                        onChanged: (v) => vmBatch.unit = v),
+                  )
+                ]),
+                Row(children: [
+                  SizedBox(
+                      height: 24.0,
+                      width: 30.0,
+                      child: Checkbox(
+                          value: vmBatch.partIsChecked,
+                          onChanged: (v) => vmBatch.partIsChecked = v)),
+                  Expanded(
+                    child: DropdownButtonFormField(
                         value: vmBatch.part,
+                        decoration: InputDecoration(
+                          labelText: "PART",
+                        ),
                         items: vmBatch.textbook.lstParts
                             .map((o) => DropdownMenuItem(
                                 value: o.value, child: Text(o.label)))
                             .toList(),
-                        decoration: InputDecoration(
-                          labelText: "PART",
-                        ),
-                        onChanged: (v) => v,
-                        onSaved: (v) => vmBatch.part = v),
-                    TextFormField(
-                      initialValue: vmBatch.seqnum.toString(),
-                      decoration: InputDecoration(
-                        labelText: "SEQNUM(+)",
-                      ),
-                      inputFormatters: [FilteringTextInputFormatter.digitsOnly],
-                      onSaved: (s) => vmBatch.seqnum = int.parse(s),
+                        onChanged: (v) => vmBatch.part = v),
+                  )
+                ]),
+                Row(children: [
+                  SizedBox(
+                      height: 24.0,
+                      width: 30.0,
+                      child: Checkbox(
+                          value: vmBatch.seqnumIsChecked,
+                          onChanged: (v) => vmBatch.seqnumIsChecked = v)),
+                  Expanded(
+                      child: TextFormField(
+                    initialValue: vmBatch.seqnum.toString(),
+                    decoration: InputDecoration(
+                      labelText: "SEQNUM(+)",
                     ),
-                    ElevatedButton(
-                        style: ElevatedButton.styleFrom(
-                            primary: Colors.blueAccent),
-                        onPressed: () {
-                          if (!_formKey.currentState.validate()) return;
-                          _formKey.currentState.save();
-                          Navigator.pop(context);
-                        },
-                        child: Text("Save",
-                            style: TextStyle(
-                              color: Colors.white,
-                            ))),
-                  ],
-                ))));
+                    inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+                    onChanged: (s) => vmBatch.seqnum = int.parse(s),
+                  ))
+                ]),
+              ],
+            )));
   }
 }
--- lib/viewmodels/words/words_unit_batch_edit_viewmodel.dart
+++ lib/viewmodels/words/words_unit_batch_edit_viewmodel.dart
@@ -4,9 +4,12 @@ import 'package:lolly_flutter/viewmodels/words/words_unit_viewmodel.dart';
 
 class WordsUnitBatchEditViewModel {
   WordsUnitViewModel vm;
-  var unit = 0;
-  var part = 0;
+  var unit = vmSettings.lstUnits[0].value;
+  var part = vmSettings.lstParts[0].value;
   var seqnum = 0;
+  var unitIsChecked = false;
+  var partIsChecked = false;
+  var seqnumIsChecked = false;
   MTextbook get textbook => vmSettings.selectedTextbook;
 
   WordsUnitBatchEditViewModel(this.vm);
